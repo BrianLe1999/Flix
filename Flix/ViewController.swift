e12e8e5892ac151e396568d0b366ad3504c0584e
Minh Nguyen Le <minhnguyenle59@berkeley.edu>
2022-02-19 13:35:33 -0800

    Initial Commit

---
+++ Flix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Flix
+//
+//  Created by 01659826174 01659826174 on 2/19/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
